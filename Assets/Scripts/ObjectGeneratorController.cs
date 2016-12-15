b29ba212c11b8e4c26674cd9bde3a21f2c480638
Szejp <janqczmowski@gmail.com>
2016-12-15 05:24:33 +0100

    Initial commit

---
+++ Assets/Scripts/ObjectGeneratorController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectGeneratorController : MonoBehaviour {
+
+    public GameObject[] _balls;
+
+    private float _waitTime = 2f;
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(GenerateObjects());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+  
+	}
+
+    IEnumerator GenerateObjects()
+    {
+        var ball = _balls[0];
+
+        while (true)
+        {
+            var position = gameObject.transform.position + new Vector3(Random.Range(-5, 5), 0, 0);
+            Instantiate(ball, position, gameObject.transform.rotation);
+            yield return new WaitForSeconds(_waitTime);
+
+
+            if (_waitTime > 0.5f)
+            {
+                _waitTime -= 0.1f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectScripts/OnTouch.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnTouch : MonoBehaviour {
+
+
+    public float _scaleFactor = 0.01f;
+    public int _countLimit = 3;
+    public GameObject _particle;
+
+    private bool isSizeChanging = false;
+    private int count = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButton(0) && !isSizeChanging)
+        {
+            StartCoroutine(ChangeSize());
+            Destroy();
+        }
+    }
+
+    IEnumerator ChangeSize()
+    {        
+        isSizeChanging = true;
+        gameObject.transform.localScale -= new Vector3(_scaleFactor, _scaleFactor, _scaleFactor);
+        count++;
+        yield return new WaitForSeconds(0.1f);
+        gameObject.transform.localScale += new Vector3(_scaleFactor, _scaleFactor, _scaleFactor);
+        isSizeChanging = false;        
+    }
+
+    void Destroy()
+    {
+        if(count == _countLimit)
+        {
+            DestroyObject(gameObject, 0.01f);
+            GameObject copy = Instantiate(_particle, gameObject.transform.position, new Quaternion()) as GameObject;
+            Destroy(copy, copy.GetComponent<ParticleSystem>().duration);
+        }
+    }
+}
